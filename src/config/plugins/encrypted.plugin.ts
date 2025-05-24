8123d5c882611b18762c32250de7a4ea4b278a5c
Abner Jurado <abner.jurado@teit.cfe.mx>
2025-05-24 12:42:41 -0600

    feat: implement interface of method compare

--- src/config/plugins/encrypted.plugin.ts
+++ src/config/plugins/encrypted.plugin.ts
@@ -1,11 +1,17 @@
-import { compare, compareSync, genSaltSync, hashSync } from "bcryptjs";
+import { compareSync, genSaltSync, hashSync } from "bcryptjs";
+
+interface OptionsCompare {
+    password: string;
+    hash: string;
+}
 
 export const encryptedPlugin = {
     hash: (password: string): string => {
         const salt = genSaltSync(12);
         return hashSync(password, salt);
     },
-    compare: (password: string, hash: string): boolean => {
+    compare: (options: OptionsCompare): boolean => {
+        const { password, hash } = options;
         return compareSync(password, hash)
     }
 }
