2465d63785e0ab6f9cf95712734ce927e3eba64b
Abner Jurado <abner.jurado@teit.cfe.mx>
2025-05-24 13:55:48 -0600

    feat: implementing the adapter pattern with the JsonWebToken package

---
+++ src/config/plugins/jwt.plugin.ts
@@ -0,0 +1,24 @@
+import jwt, { SignOptions } from "jsonwebtoken";
+import { envPlugin } from "./env.plugin";
+
+const JWT_SECRET = envPlugin.JWT_SECRET;
+
+export class jwtConfig {
+
+    static async generateToken(payload: any, duration: SignOptions["expiresIn"] = '1h') {
+        return new Promise((resolve) => {
+            jwt.sign(payload, JWT_SECRET, { expiresIn: duration }, (err, token) => {
+
+                if (err) return resolve(null);
+
+                resolve(token)
+
+            });
+        })
+    }
+
+    static validateToken(token: string) {
+        // TODO: Validate token
+        return;
+    }
+}
