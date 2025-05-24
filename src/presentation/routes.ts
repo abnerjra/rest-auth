2f1a0fdde141b4cb286d8c7a78b82683fb24a270
Abner Jurado <abner.jurado@teit.cfe.mx>
2025-05-24 12:05:37 -0600

    feat: definition of application routes

--- src/presentation/routes.ts
+++ src/presentation/routes.ts
@@ -1,11 +1,14 @@
 import { Router } from "express";
+import { Authroutes } from "./auth/routes";
 
 export class AppRoutes {
     
     static get routes(): Router {
         const router = Router();
+        const prefix = '/api';
 
         // Define your routes here
+        router.use(`${prefix}/auth`, Authroutes.routes);
         
 
         return router;
