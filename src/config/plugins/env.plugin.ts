09f1109aac01d3d325a2b72f38d5e33d2016e615
Abner Jurado <abner.jurado@teit.cfe.mx>
2025-05-23 19:39:42 -0600

    feat: mapping new variables

--- src/config/plugins/env.plugin.ts
+++ src/config/plugins/env.plugin.ts
@@ -3,4 +3,6 @@ import * as env from 'env-var';
 
 export const envPlugin = {
     PORT: env.get('PORT').required().asPortNumber(),
+    MONGO_URL: env.get('MONGO_URL').required().asString(),
+    MONGO_DB_NAME: env.get('MONGO_DB_NAME').required().asString(),
 }
