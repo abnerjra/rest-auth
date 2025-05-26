bc2ba8037d0a01a4278151a899bb9fc3bfdd31d2
Abner Jurado <abner.jurado@teit.cfe.mx>
2025-05-26 10:15:21 -0600

    feat: mapping new environment variables

--- src/config/plugins/env.plugin.ts
+++ src/config/plugins/env.plugin.ts
@@ -6,4 +6,7 @@ export const envPlugin = {
     MONGO_URL: env.get('MONGO_URL').required().asString(),
     MONGO_DB_NAME: env.get('MONGO_DB_NAME').required().asString(),
     JWT_SECRET: env.get('JWT_SECRET').required().asString(),
+    MAILER_SERVICE: env.get('MAILER_SERVICE').required().asString(),
+    MAILER_EMAIL: env.get('MAILER_EMAIL').required().asString(),
+    MAILER_SECRET_KEY: env.get('MAILER_SECRET_KEY').required().asString(),
 }
