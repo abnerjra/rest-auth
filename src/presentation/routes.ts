1a54b82defbe34be5ab01fe8edfaf4f3c4bcc970
Abner Jurado <abner.jurado@teit.cfe.mx>
2025-05-27 10:48:19 -0600

    feat: product routing call

--- src/presentation/routes.ts
+++ src/presentation/routes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { Authroutes } from "./auth/routes";
 import { CategoryRoutes } from "./category/routes";
+import { ProductRoutes } from "./product/routes";
 
 export class AppRoutes {
     
@@ -10,8 +11,8 @@ export class AppRoutes {
 
         // Define your routes here
         router.use(`${prefix}/auth`, Authroutes.routes);
-        router.use(`${prefix}/category`, CategoryRoutes.routes);
-        
+        router.use(`${prefix}/categories`, CategoryRoutes.routes);
+        router.use(`${prefix}/products`, ProductRoutes.routes);
 
         return router;
     }
