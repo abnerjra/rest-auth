d4d659d2888013f8e93a519dca34bf3b0ece5e87
Abner Jurado <abner.jurado@teit.cfe.mx>
2025-05-27 15:37:08 -0600

    feat: definition of product routes

--- src/presentation/product/routes.ts
+++ src/presentation/product/routes.ts
@@ -1,12 +1,14 @@
 import { Router } from "express";
 import { ProductController } from "./controller";
 import { AuthMiddleware } from "../middlewares";
+import { ProductService } from "../services/product.service";
 
 export class ProductRoutes {
     static get routes(): Router {
         const router = Router();
 
-        const controller = new ProductController()
+        const service = new ProductService
+        const controller = new ProductController(service)
 
         const middlewares = [AuthMiddleware.validateToken]
 
