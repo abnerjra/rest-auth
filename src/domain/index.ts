f65040dec1a575ab3c7497a41859405fb74c8ec5
Abner Jurado <abner.jurado@teit.cfe.mx>
2025-05-28 10:22:22 -0600

    feat: file updated

--- src/domain/index.ts
+++ src/domain/index.ts
@@ -2,12 +2,14 @@
 // Auth
 export * from './dtos/auth/login-user.dto'
 export * from './dtos/auth/register-user.dto'
-export * from './dtos/shared/pagination.dto'
 
 // Category
 export * from './dtos/category/create-category.dto'
 export * from './dtos/category/update-category.dto'
+
+// Product
 export * from './dtos/product/create-product.dto'
+export * from './dtos/product/update-product.dto'
 
 // TODO : ENTITIES
 export * from './entities/user.entity'
@@ -15,3 +17,7 @@ export * from './entities/category.entity'
 
 // TODO: CUSTOM
 export * from './errors/custom.error'
+
+// TODO: SHARE
+export * from './dtos/shared/pagination.dto'
+export * from './dtos/shared/read-by-id.dto'
