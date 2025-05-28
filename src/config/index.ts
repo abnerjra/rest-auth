3ece88c413dd3a83a2a217731c58147d6d94a5a2
Abner Jurado <abner.jurado@teit.cfe.mx>
2025-05-28 12:27:00 -0600

    feat: file updated

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,4 +2,5 @@ export * from "./plugins/encrypted.plugin";
 export * from "./plugins/env.plugin";
 export * from "./plugins/jwt.plugin";
 export * from "./plugins/regular-exp";
+export * from "./plugins/uuid.pluging"
 export * from "./plugins/validators"
